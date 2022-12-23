a8a5129645df9e84e222931354c3085900595803
Pavel Suspicin <pavelsuspicin@gmail.com>
2022-12-23 14:03:49 +0300

    23.12.22 14:00

--- script.js
+++ script.js
@@ -19,24 +19,32 @@ class MyArray {
   // Возвращает значение по индексу.
   // Если индекс за пределами — кидает ошибку.
   get(index) {
-    // ...
+    if (index > this.size - 1) {
+      throw new Error('Индекс за пределами вызова')
+    }
+    return this.memory[index]
   }
 
   // Устанавливает значение по индексу.
   // Если индекс за пределами — кидает ошибку.
   set(index, value) {
-    // ...
+    if (index > this.size - 1) throw new Error('Индекс за пределами вызова')
+    return (this.memory[index] = value)
   }
 
-  // Добавляет новый элемент в массив.
-  // Если index не определён — добавляет в конец массива.
   // В противном случае — добавляет по индексу со сдвигом
   // всех последующих элементов.
-  // Если индекс за пределами - кидает ошибку.
   // Увеличивает выделенную память вдвое, если необходимо.
   // Возвращает новую длину массива.
   add(value, index) {
-    // ...
+    this.length = index + 1
+    // Если индекс за пределами - кидает ошибку.
+    if (index > this.size - 1) throw new Error('Индекс за пределами')
+    // Если index не определён — добавляет в конец массива.
+    if (index === undefined) this.memory[this.size - 1] = value
+    // Добавляет новый элемент в массив.
+    else this.memory[index] = value
+    return this.memory
   }
 
   // Удаляет элемент по индексу со сдвигом всех последующих элементов.
@@ -49,15 +57,12 @@ class MyArray {
 
 function allocate(size) {
   const memory = {}
-
   for (let i = 0; i < size; i++) {
     memory[i] = undefined
   }
-
   return memory
 }
-
-const arr = []
-console.log(arr)
 const myArr = new MyArray(2)
 console.log(myArr)
+console.log(myArr.add(1, 0))
+console.log(myArr.length)
